6a69d70823e554c95566b58d101a0f0bf81e2599
Süleyman Kaplan <149884529+suleymankaplan@users.noreply.github.com>
2024-09-08 20:30:54 +0300

    File update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
